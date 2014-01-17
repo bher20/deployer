842ab5f047c45087d13917f396f94f2d498b48df
Brad Herring <bradherring@bherville.com>
2014-01-17 13:01:19 -0500

    Added: New method to check if a version exists.

--- lib/deployer/deployment.rb
+++ lib/deployer/deployment.rb
@@ -5,12 +5,14 @@ require 'fileutils'
 require 'time'
 
 class Deployment
-  def initialize(deploy_destination, repository, version, name, logger)
+  def initialize(deploy_destination, repository, version, name, logger, working_copy_dir)
     @@deploy_destination = deploy_destination
     @@repository = repository
     @@version = version
     @@name = name
     @@logger = logger
+    @@working_copy_dir = working_copy_dir
+    @@working_copy = prepare_working_copy working_copy_dir
   end
 
   def prepare_working_copy (working_copy)
@@ -54,22 +56,30 @@ class Deployment
     wc.checkout tag
   end
 
-  def deploy (deploy_source, working_copy_dir, force = false)
-    working_copy = prepare_working_copy working_copy_dir
+  def version?(tag_name)
+    #Check to see if tag already exists
+    begin
+      @@working_copy.tag(tag_name)
+      return true
+    rescue
+      return false
+    end
+  end
 
+  def deploy (deploy_source, force = false)
     @@logger.info { "Deploying..." }
 
     @@logger.info { "Coping files into working copy" }
-    FileUtils.cp_r "#{deploy_source}/.", working_copy_dir, :remove_destination=>true
+    FileUtils.cp_r "#{deploy_source}/.", @@working_copy_dir, :remove_destination=>true
 
 
     #Add the new files to the repo
     @@logger.info { "Adding new files to git in the working copy" }
-    working_copy.add
+    @@working_copy.add
 
     begin
       @@logger.info { "Commiting changes" }
-      working_copy.commit_all("Updated: Release #{@@version}")
+      @@working_copy.commit_all("Updated: Release #{@@version}")
     rescue Exception => e
       @@logger.warn { "No new were detected, not commiting" }
 
@@ -82,16 +92,17 @@ class Deployment
     @@logger.info { "Creating tag #{tag_name}" }
 
     #Check to see if tag already exists
-    begin
-      working_copy.tag(tag_name)
+    if (version? tag_name)
+      @@working_copy.tag(tag_name)
 
       @@logger.warn { "Tag #{tag_name} already exists, not creating." }
-    rescue
-      working_copy.add_tag("#{tag_name}")
+
+    else
+      @@working_copy.add_tag("#{tag_name}")
     end
 
     @@logger.info { "Pushing updates to the origin master with the tag" }
-    working_copy.push 'origin', 'master', true
+    @@working_copy.push 'origin', 'master', true
 
 
     update @@version
